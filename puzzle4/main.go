5fa4f48698b48d0d43280c3fa2060e8b00104ef4
Hayden Braxton <hayden.braxton@everactive.com>
2022-12-05 21:51:32 -0500

    day 4, part 2

--- puzzle4/main.go
+++ puzzle4/main.go
@@ -22,25 +22,18 @@ func main() {
 			return sum
 		}
 	})
+
+	overlapCounts := util.Fold(0, input, func(sum int, pair string) int {
+		if doPairsOverlap(pair) {
+			return sum + 1
+		} else {
+			return sum
+		}
+	})
+
 	fmt.Println(subsetCounts)
+	fmt.Println(overlapCounts)
 }
-
-/*
-2-3
-1-4
-
-a.first >= b.first?
-	1 >= 4?
-
-1-4
-4-7
-
-4-7
-1-4
-
-1234.....
-.23......
-*/
 
 func arePairsSubsets(pair string) bool {
 	split := strings.Split(pair, ",")
@@ -53,7 +46,19 @@ func arePairsSubsets(pair string) bool {
 	c, _ := strconv.Atoi(range2[0])
 	d, _ := strconv.Atoi(range2[1])
 
-	fmt.Println(a, b, c, d)
-
 	return (a <= c && b >= d) || (a >= c && b <= d)
+}
+
+func doPairsOverlap(pair string) bool {
+	split := strings.Split(pair, ",")
+
+	range1 := strings.Split(split[0], "-")
+	range2 := strings.Split(split[1], "-")
+
+	a, _ := strconv.Atoi(range1[0])
+	b, _ := strconv.Atoi(range1[1])
+	c, _ := strconv.Atoi(range2[0])
+	d, _ := strconv.Atoi(range2[1])
+
+	return (a <= d && b >= c) || (a >= d && b <= c)
 }
